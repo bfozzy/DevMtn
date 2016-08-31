cf5be9f1f6af3b98d964bf7243c10290e19bcfd3
Brad Frost <bfozzy@icloud.com>
2016-08-31 13:44:52 -0600

    committing for morning project. Starting afternoon project

--- week3/8-31-wed/mini-userProfiles2-master/controller.js
+++ week3/8-31-wed/mini-userProfiles2-master/controller.js
@@ -1,10 +1,23 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
+  $scope.pageNum = 1;
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers($scope.pageNum).then(function(response) {
+      $scope.users = response.data.data;
+      $scope.totalPages = response.data.total_pages;
+    });
+  }
+  $scope.getUsers();
+  $scope.prev = function(){
+    if($scope.pageNum > 1){
+    $scope.pageNum--
+    $scope.getUsers();
+  }
+  }
+  $scope.next = function(){
+    if ($scope.totalPages > $scope.pageNum)
+    $scope.pageNum++
+    $scope.getUsers();
   }
 
-
-  $scope.getUsers();
-
 });
+// var promise = mainService.getUsers();
