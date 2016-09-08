0a685abfced1d5d644e0b19c7426ae950923f055
Brad Frost <bfozzy@icloud.com>
2016-09-08 16:44:26 -0600

    saving project

--- noServerProject/js/bio/bioDirective.js
+++ noServerProject/js/bio/bioDirective.js
@@ -1,1 +1,25 @@
 angular.module("app")
+    .directive("bioDirective", function() {
+        return {
+            templateUrl: "js/bio/bioDirectiveTemplate.html",
+            //template: '<input ng-model="data" type="text"/>'
+            restrict: "AE",
+            // scope: {
+            //     personData: '='
+            // },
+            link: function(scope, element, attributes) {
+                //here I could include a function set to scope that has an ng-hover or click to expand text for each person image.
+
+
+            },
+            controller: function($scope, mainService) {
+                //pull in an array of objects with each person in the band that has a little bio on them
+
+
+                //working
+            }
+
+
+
+        }
+    });
---
+++ noServerProject/js/music/musicDirective.js
@@ -0,0 +1,25 @@
+angular.module("app")
+    .directive("musicDirective", function() {
+        return {
+            templateUrl: "js/music/music.html",
+            //template: '<input ng-model="data" type="text"/>'
+            restrict: "AE",
+            // scope: {
+            //     personData: '='
+            // },
+            link: function(scope, element, attributes) {
+                //here I could include a function set to scope that has an ng-hover or click to expand text for each person image.
+
+
+            },
+            controller: function($scope, mainService) {
+                //pull in an array of objects with each person in the band that has a little bio on them
+
+
+                //working
+            }
+
+
+
+        }
+    });
